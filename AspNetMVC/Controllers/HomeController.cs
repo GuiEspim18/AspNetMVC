ea5a007c0a4a3b03ad41361a55cc7684ccbf0813
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-04-10 11:03:51 -0300

    home controller

--- AspNetMVC/Controllers/HomeController.cs
+++ AspNetMVC/Controllers/HomeController.cs
@@ -1,31 +1,32 @@
-﻿using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using AspNetMVC.Models;
+using Microsoft.Extensions.Logging;
 
-namespace AspNetMVC.Controllers;
-
-public class HomeController : Controller
+namespace AspNetMVC.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    [Route("[controller]")]
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
     }
 }
